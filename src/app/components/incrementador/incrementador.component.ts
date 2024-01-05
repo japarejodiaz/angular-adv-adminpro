b157ede3bd0e1efb3f30d1352c54f65318e72c66
Jesus Parejo <japarejo.diaz@gmail.com>
2024-01-05 02:15:46 -0300

    2023-12-07: Se resplada el proyecto 2

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styles: [
+  ]
+})
+export class IncrementadorComponent {
+
+  @Input('valor') progreso : number = 90;
+
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter();
+
+  cambiarValor (valor: number) {
+
+    if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
+      return this.progreso = 100;
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
+      return this.progreso = 0;
+    }
+
+    this.progreso = this.progreso + valor;
+    this.valorSalida.emit( this.progreso );
+    return this.progreso;
+  }
+
+
+}
